e3ba298a7b60fe756d1b1c1b0296e962376dca32
aldinnasrun <adnzain40@gmail.com>
2024-07-26 09:13:08 +0700

    add readme

--- md/predict_cervical_cancer.py
+++ md/predict_cervical_cancer.py
@@ -1,5 +1,4 @@
 def predict_cervical_cancer(age, age_at_first_menstruation, pelvic_pain, lower_abdomen_pain, weight_loss, fatigue, discharge_color, bleeding_outside_cycle, bleeding_duration, abdominal_mass):
-    # Dummy prediction logic (to be replaced with actual model)
     if age > 50 or pelvic_pain == 2 or lower_abdomen_pain == 2 or weight_loss == 2 or fatigue == 2 or discharge_color == 3 or bleeding_outside_cycle == 2 or bleeding_duration > 1 or abdominal_mass == 2:
         if age > 50 or abdominal_mass == 2:
             return "Terdeteksi penyakit kanker serviks tahap Lanjut"
